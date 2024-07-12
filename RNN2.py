c6132b52036bb014e3143a4ea883a6e7c7cdea38
Yovav Cohen Tene <yovavc@gmail.com>
2024-07-12 18:45:32 +0300

    Updating network

--- RNN2.py
+++ RNN2.py
@@ -37,35 +37,33 @@ class BidirectionalRNN(nn.Module):
 
 
 class RNNAttention(nn.Module):
-    def __init__(self, catagories, rate=16000, length=16000):
+    def __init__(self, catagories):
         super(RNNAttention, self).__init__()
-        sr = rate
-        iLen = length
 
-        kernel_size1 = (5, 1)
+        kernel_size1 = (5, 3)
         conv_padding = reduce(__add__,
                               [(k // 2 + (k - 2 * (k // 2)) - 1, k // 2) for k in kernel_size1[::-1]])
         self.zp1 = nn.ZeroPad2d(conv_padding)
-        self.c1 = nn.Conv2d(128, 10, kernel_size1)
-        self.bn1 = nn.BatchNorm2d(10)
+        self.c1 = nn.Conv2d(128, 32, kernel_size1)
+        self.bn1 = nn.BatchNorm2d(32)
 
-        kernel_size2 = (5, 1)
+        kernel_size2 = (5, 3)
         conv_padding = reduce(__add__,
                               [(k // 2 + (k - 2 * (k // 2)) - 1, k // 2) for k in kernel_size2[::-1]])
         self.zp2 = nn.ZeroPad2d(conv_padding)
-        self.c2 = nn.Conv2d(10, 1, kernel_size2)
-        self.bn2 = nn.BatchNorm2d(1)
+        self.c2 = nn.Conv2d(32, 16, kernel_size2)
+        self.bn2 = nn.BatchNorm2d(16)
 
         # x = Reshape((125, 80)) (x)
         # keras.backend.squeeze(x, axis)
         # self.RNN1 = BidirectionalRNN(81, 100).to(device)
         # self.RNN2 = BidirectionalRNN(100, 128).to(device)
-        self.RNN1 = nn.LSTM(9, 50, 3, bidirectional=True, batch_first=True)
-        self.RNN2 = nn.LSTM(90, 64, 2, bidirectional=True, batch_first=True)
+        self.RNN1 = nn.LSTM(81, 64, 3, bidirectional=True, batch_first=True)
+        self.RNN2 = nn.LSTM(32, 64, 2, bidirectional=True, batch_first=True)
         self.WQ = nn.Linear(128, 128)
 
         self.lin1 = nn.Linear(128, 64)
-        self.lin2 = nn.Linear(64, 35)
+        self.lin2 = nn.Linear(64, catagories)
 
     def forward(self, x):
         b = x.shape[0]
@@ -80,13 +78,12 @@ class RNNAttention(nn.Module):
         x = F.relu(x)
         x = self.bn2(x)
 
-        x = torch.squeeze(x, -1).transpose(1, 2)
-        x = torch.reshape(x, (b, 9, 9))
+        x = torch.squeeze(x, -1)
         x, _ = self.RNN1(x)
-        x = torch.reshape(x, (b, 10, 90))
+        x = torch.reshape(x, (b, 64, 32))
         x, _ = self.RNN2(x)
-
         x1 = x[:, -1, :]
+
         Q = self.WQ(x1)
         att = torch.einsum("bij,bj->bi", x, Q)
         att = torch.softmax(att, -1)
